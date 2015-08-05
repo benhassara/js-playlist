23c44878d79745ff0fca2d22de86914175331a96
benhassara <bhassara@gmail.com>
2015-08-05 17:43:12 -0600

    built out Playlist and Song basics

--- js/playlist.js
+++ js/playlist.js
@@ -1,3 +1,40 @@
-// add scripts
 
-console.log("sanity check!");
+//Playlist class
+function Playlist(name) {
+  this.name = name;
+  this.songs = [];
+  this.currentSong = null;
+}
+
+Playlist.prototype.addSong = function(song) {
+  //add a song to the Playlist
+  //song is a Song obj
+
+  this.songs.push(song);
+};
+
+Playlist.prototype.find = function(song) {
+  //find the song in the Playlist, return loc
+  var index = -1;
+  this.songs.forEach(function(q, i){
+    if (q.title === song.title && q.artist === song.artist)
+      index = i;
+  });
+
+  return index;
+};
+
+Playlist.prototype.getArtists = function(){
+  //get all artists in the Playlist
+  return this.songs.map(function(a){return a.artist;});
+};
+
+Playlist.prototype.currentlyPlaying = function() {
+  //return currently playing song
+  return this.songs[this.currentSong];
+};
+
+Playlist.prototype.play = function() {
+  //start the playlist
+  this.currentSong = 0;
+};
---
+++ js/song.js
@@ -0,0 +1,7 @@
+
+//Song class
+function Song(title, artist, length) {
+  this.title = title;
+  this.artist = artist;
+  this.length = length;
+}
